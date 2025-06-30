38a6c3209a6d7f16d3168504a3fe95da31668b97
LMathan <smartinvention1434@gmail.com>
2025-06-30 09:34:03 +0530

    api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,4 @@
 // netlify/functions/api.ts
 import serverless from "serverless-http";
-import app from "../../src/index";
+import app from "../../server/index";
 export const handler = serverless(app);
